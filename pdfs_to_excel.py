f51529dad6b71991dcfa69616f0a5d2a42c3fd87
ritwiktrivedi <ritwik.trivedi@gmail.com>
2025-03-01 00:23:42 +0530

    Corrected the pdfs_to_excel code as it was not working. It kind of works but the excel columns are shifting but can fix with simple onetime copy paste.

--- pdfs_to_excel.py
+++ pdfs_to_excel.py
@@ -3,19 +3,30 @@ import pdfplumber
 import os
 
 # Get all PDF files in the directory
-pdf_files = [f for f in os.listdir('./mock_roe_4') if f.endswith('.pdf')]
+directory_path = './mock4/mock_roe_4/'
+pdf_files = [f for f in os.listdir(directory_path) if f.endswith('.pdf')]
 
 dfs = []
-for i in pdf_files:
-    with pdfplumber.open(f"./mock_roe_4/{i}") as pdf:
-        for i, page in enumerate(pdf.pages):
-            table = page.extract_table()
-            if table:  # Check if a table is found
-                df = pd.DataFrame(table)  # Convert to DataFrame
-                dfs.append(df)
+for filename in pdf_files:
+    file_path = os.path.join(directory_path, filename)
+    if os.path.exists(file_path):
+        with pdfplumber.open(file_path) as pdf:
+            for page in pdf.pages:
+                table = page.extract_table()
+                if table:  # Check if a table is found
+                    if not dfs:
+                        df = pd.DataFrame(table[1:], columns=table[0])  # Set the header row as column names
+                    else:
+                        df = pd.DataFrame(table[1:])  # Exclude the header row
+                    dfs.append(df)
+    else:
+        print(f"File not found: {file_path}")
 
 # Concatenate all DataFrames into a single one
 df_all = pd.concat(dfs, ignore_index=True)
 
 # Save the DataFrame to an Excel file
-df_all.to_excel('./processed_files/all_tables.xlsx', index=False)
+output_directory = './processed_files/'
+os.makedirs(output_directory, exist_ok=True)
+df_all.to_excel(os.path.join(output_directory, 'all_tables.xlsx'), index=False)
+
--- readme.md
+++ readme.md
@@ -32,6 +32,7 @@ https://www.adobe.com/in/acrobat/online/pdf-to-excel.html
 - After conversion handle page breakpoints (last row on every page may get split in two). Delete the extra row for this.
 - Then copy the entire content in a new sheet and paste as values. (this helps is converting to csv.)
 
-Alternative: [pdf_to_excel.py](./pdf_to_excel.py)
+Alternative: [pdf_to_excel.py](./pdf_to_excel.py)  
+For multiple pdfs: [pdfs_to_excel.py](./pdfs_to_excel.py)
 
 ## Parsing HTML
